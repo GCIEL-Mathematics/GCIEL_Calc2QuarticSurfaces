1c3e7d4ec37d713295b35841f5a8e9e939008114
cpfrench <frenchc@grinnell.edu>
2023-05-29 17:27:42 -0500

    Incorporated lock plane script and eliminated teleport option

--- Assets/VR Plane Manipulation/LockPlane.cs
+++ Assets/VR Plane Manipulation/LockPlane.cs
@@ -36,6 +36,23 @@ public class LockPlane : MonoBehaviour
         }
 
         grabInteractable.trackRotation = false;
+        Quaternion newRot = this.transform.rotation;
+
+        switch (current)
+        {
+            case LockState.XY:
+                newRot = Quaternion.AngleAxis(0, Vector3.up);
+                break;
+            case LockState.XZ:
+                newRot = Quaternion.AngleAxis(90, Vector3.forward);
+                break;
+            case LockState.YZ:
+                newRot = Quaternion.AngleAxis(90, Vector3.left);
+                break;
+        }
+
+        // the position setting doesn't work properly but it's good enough
+        this.transform.SetPositionAndRotation(grabInteractable.interactorsSelecting[0].transform.position, newRot);
         // todo: lock properly
     }
 }
